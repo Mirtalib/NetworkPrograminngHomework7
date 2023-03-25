2d8a87deb76bbfe52e1a301e707ac5142fed98b1
Mirtalib <108764438+Mirtalib@users.noreply.github.com>
2023-03-25 14:29:45 +0400

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+var listener = new HttpListener();
+
+listener.Prefixes.Add(@"http://localhost:27001/");
+listener.Prefixes.Add(@"http://localhost:27002/");
+
+listener.Start();
+Console.WriteLine("Listener...");
+
+while (true)
+{
+    var context = await listener.GetContextAsync();
+
+    var request = context.Request;
+    var response = context.Response;
+
+    
+
+    var writer = new StreamWriter(response.OutputStream);
+    
+    // await writer.Write(,)  
+}
+
